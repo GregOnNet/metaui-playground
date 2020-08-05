7512464057290586aea22a1187fda08aa99d458c
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-08-05 11:56:11 +0200

    fix: remove change detectorRef

--- apps/metaui-playground/src/app/star-wars/views/character-edit.component.ts
+++ apps/metaui-playground/src/app/star-wars/views/character-edit.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { pluck, switchMap } from 'rxjs/operators';
@@ -7,36 +7,31 @@ import { Character } from '../model/character';
 
 @Component({
   selector: 'mp-character-form',
-  template: `    
+  template: `
     <h2>Edit Character</h2>
 
     <ng-container *ngIf="character$ | async as character">
-        <m-context [object]="character" operation="edit" layout="Inspect">
-            <m-include-component></m-include-component>
-        </m-context>
-        <button mat-raised-button color="primary">SAVE</button>
+      <m-context [object]="character" operation="edit" layout="Inspect">
+        <m-include-component></m-include-component>
+      </m-context>
+      <button mat-raised-button color="primary">SAVE</button>
     </ng-container>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  styles: [],
-
+  styles: []
 })
 export class CharacterEditComponent {
   character$ = this.fetchCharacter();
 
   constructor(
     private route: ActivatedRoute,
-    private characterApi: CharacterApi,
-    private cd: ChangeDetectorRef
+    private characterApi: CharacterApi
   ) {}
 
   private fetchCharacter(): Observable<Character | null> {
     return this.route.params.pipe(
       pluck('id'),
-      switchMap(id => this.characterApi.fetchById(id)),
+      switchMap(id => this.characterApi.fetchById(id))
     );
-
-    this.cd.detectChanges();
-
   }
 }
