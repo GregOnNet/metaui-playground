7d9401e3e42225d79ce40dd87afed6098c922d7c
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-08-05 12:44:40 +0200

    feat: spend a few methods to the service

--- apps/metaui-playground/src/app/star-wars/api/character-api.service.ts
+++ apps/metaui-playground/src/app/star-wars/api/character-api.service.ts
@@ -1,18 +1,45 @@
 import { Injectable } from '@angular/core';
-import { defer, Observable, of } from 'rxjs';
-import { characterMocks } from './character.mock';
+import { Observable, of } from 'rxjs';
 import { Character } from '../model/character';
+import { characterMocks } from './character.mock';
 
 @Injectable({ providedIn: 'root' })
 export class CharacterApi {
+  private characters = characterMocks;
+
   fetchAll(): Observable<Character[]> {
-    return defer(() => of(characterMocks));
+    return of(this.characters);
   }
 
   fetchById(id: number | string): Observable<Character | null> {
-    return defer(() => {
-      const found = characterMocks.find(character => character.id === +id);
-      return of(found || null);
-    });
+    const found = this.characters.find(character => character.id === +id);
+    return of(found || null);
+  }
+
+  update(characterUpdate: Partial<Character>) {
+    const characterOrigin = this.characters.find(
+      character => character.id === characterUpdate.id
+    );
+
+    if (!characterOrigin) {
+      throw new Error(
+        `[Character API] Unable to find character with id "${characterUpdate.id}"`
+      );
+    }
+
+    Object.assign(characterOrigin, characterUpdate);
+
+    this.characters = this.characters.map(character =>
+      character.id === characterUpdate.id
+        ? Object.assign(character, characterUpdate)
+        : character
+    );
+
+    return of(characterOrigin);
+  }
+
+  remove(id: string) {
+    this.characters = this.characters.filter(character => character.id !== +id);
+    return of(true);
   }
 }
