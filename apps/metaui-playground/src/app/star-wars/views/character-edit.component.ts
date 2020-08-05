dc945a4c36c77eb525dd81f3e84ac8c1a12295f7
fkolar <fkolar@icloud.com>
2020-08-05 11:37:18 +0200

    chore: Bump up metaui rules version to 9.0.4

--- apps/metaui-playground/src/app/star-wars/views/character-edit.component.ts
+++ apps/metaui-playground/src/app/star-wars/views/character-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { pluck, switchMap } from 'rxjs/operators';
@@ -7,9 +7,7 @@ import { Character } from '../model/character';
 
 @Component({
   selector: 'mp-character-form',
-  template: `
-    
-    
+  template: `    
     <h2>Edit Character</h2>
 
     <ng-container *ngIf="character$ | async as character">
@@ -19,20 +17,26 @@ import { Character } from '../model/character';
         <button mat-raised-button color="primary">SAVE</button>
     </ng-container>
   `,
-  styles: []
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [],
+
 })
 export class CharacterEditComponent {
   character$ = this.fetchCharacter();
 
   constructor(
     private route: ActivatedRoute,
-    private characterApi: CharacterApi
+    private characterApi: CharacterApi,
+    private cd: ChangeDetectorRef
   ) {}
 
   private fetchCharacter(): Observable<Character | null> {
     return this.route.params.pipe(
       pluck('id'),
-      switchMap(id => this.characterApi.fetchById(id))
+      switchMap(id => this.characterApi.fetchById(id)),
     );
+
+    this.cd.detectChanges();
+
   }
 }
